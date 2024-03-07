c6e900f2a8bfd7b24c5c9a3742c31b5cd0a509fa
hrushikesh-joshi <46347619+hrushikesh-joshi@users.noreply.github.com>
2024-03-07 13:14:24 -0500

    Create utils.py

---
+++ resume parser/utils.py
@@ -0,0 +1,101 @@
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import io
+import spacy
+from spacy.matcher import Matcher
+import re
+import yake
+import pandas as pd
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as fh:
+        # iterate over all pages of PDF document
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            # creating a resoure manager
+            resource_manager = PDFResourceManager()
+            
+            # create a file handle
+            fake_file_handle = io.StringIO()
+            
+            # creating a text converter object
+            converter = TextConverter(
+                                resource_manager, 
+                                fake_file_handle, 
+                                codec='utf-8', 
+                                laparams=LAParams()
+                        )
+
+            # creating a page interpreter
+            page_interpreter = PDFPageInterpreter(
+                                resource_manager, 
+                                converter
+                            )
+
+            # process current page
+            page_interpreter.process_page(page)
+            
+            # extract text
+            text = fake_file_handle.getvalue()
+            yield text
+
+            # close open handles
+            converter.close()
+            fake_file_handle.close()
+        return
+
+def extract_name(resume_text):
+
+    nlp = spacy.load('en_core_web_sm')
+
+    # initialize matcher with a vocab
+    matcher = Matcher(nlp.vocab)
+
+    nlp_text = nlp(resume_text)
+    
+    # First name and Last name are always Proper Nouns
+    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+    
+    matcher.add('NAME', [pattern])
+    
+    matches = matcher(nlp_text)
+    
+    for match_id, start, end in matches:
+        span = nlp_text[start:end]
+        return span.text
+
+def extract_mobile_number(text):
+    phone = re.findall(re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), text)
+    
+    if phone:
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
+
+def extract_email(email):
+    email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", email)
+    if email:
+        try:
+            return email[0].split()[0].strip(';')
+        except IndexError:
+            return None
+
+def extract_keywords(text):
+    kw_extractor = yake.KeywordExtractor()
+    language = "en"
+    max_ngram_size = 1
+    deduplication_threshold = 0.9
+    numOfKeywords = 40
+    custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+    keywords = custom_kw_extractor.extract_keywords(text)
+    data = pd.read_csv("skills.csv") 
+    skills = list(data.columns.values)
+    skillset = []
+    for token in keywords:
+        if token[0].lower() in skills:
+            skillset.append(token)
+    return skillset[:5]

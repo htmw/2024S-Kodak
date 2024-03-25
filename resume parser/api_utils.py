6b72d4729aab5066a94d64b7178f150a878f28e2
hrushikesh-joshi <46347619+hrushikesh-joshi@users.noreply.github.com>
2024-03-24 21:15:31 -0400

    Updated this file to be used as a template

--- resume parser/api_utils.py
+++ resume parser/api_utils.py
@@ -4,10 +4,10 @@ import json
 # function returns a list dictionaries of jobs
 
 def get_job_list(keywords):
-    basic = requests.auth.HTTPBasicAuth('8388c68f-19f9-417a-a0c4-05f1ddb1c5f4', '')
+    basic = '' # From config
     payload, headers = ""
     #test version of  API call, should return a list jobs
-    url = "https://www.reed.co.uk/api/1.0/search?keywords=software developer"       # add keywords here
+    url = ''       # add url from config and keywords from api  here
     response = requests.request("GET", url, data=payload, headers=headers, auth = basic)
     job_list = dict()
     job_list = json.loads(response.text)
@@ -18,7 +18,7 @@ def get_job_list(keywords):
 # function returns details of a single job based on the jobId passed in
 
 def get_job_details(job_id):
-    url = "https://www.reed.co.uk/api/1.0/jobs/52240617"            # replace the number at the end with jobId
+    url = ''            # add url from config and replace the number at the end with jobId
     response = requests.request("GET", url, headers=headers, auth = basic)
     job = json.loads(response.text)
     return job

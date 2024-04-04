1ad2c154c7e5bbb721af0f8a3a51de58222964cc
hrushikesh-joshi <46347619+hrushikesh-joshi@users.noreply.github.com>
2024-04-04 13:05:09 -0400

    Update utils.py

--- resume parser/utils.py
+++ resume parser/utils.py
@@ -1,50 +1,15 @@
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-import io
 import spacy
 from spacy.matcher import Matcher
 import re
 import yake
 import pandas as pd
 
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as fh:
-        # iterate over all pages of PDF document
-        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
-            # creating a resoure manager
-            resource_manager = PDFResourceManager()
-            
-            # create a file handle
-            fake_file_handle = io.StringIO()
-            
-            # creating a text converter object
-            converter = TextConverter(
-                                resource_manager, 
-                                fake_file_handle, 
-                                codec='utf-8', 
-                                laparams=LAParams()
-                        )
-
-            # creating a page interpreter
-            page_interpreter = PDFPageInterpreter(
-                                resource_manager, 
-                                converter
-                            )
-
-            # process current page
-            page_interpreter.process_page(page)
-            
-            # extract text
-            text = fake_file_handle.getvalue()
-            yield text
-
-            # close open handles
-            converter.close()
-            fake_file_handle.close()
-        return
+def calculate_score(doc1, doc2):
+    nlp = spacy.load('en_core_web_lg')
+    doc1 = nlp(doc1)
+    doc2 = nlp(doc2)
+    score = doc1.similarity(doc2)
+    return score
 
 def extract_name(resume_text):
 
@@ -90,12 +55,12 @@ def extract_keywords(text):
     max_ngram_size = 1
     deduplication_threshold = 0.9
     numOfKeywords = 40
-    custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+    custom_kw_extractor = kw_extractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
     keywords = custom_kw_extractor.extract_keywords(text)
     data = pd.read_csv("skills.csv") 
     skills = list(data.columns.values)
     skillset = []
     for token in keywords:
         if token[0].lower() in skills:
-            skillset.append(token[0])
+            skillset.append(token)
     return skillset[:5]

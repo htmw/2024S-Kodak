b738d92dd0449a510e3646245b0d954c4f8f1a89
root <root@ip-172-31-20-170.ec2.internal>
2024-05-04 19:18:22 +0000

    created docker-compose to resolve service networking issue
     Committer: Sujay Sail <sujaysail10@gmail.com>

--- Sprint 4/flask api/config.py
+++ Sprint 4/flask api/config.py
@@ -13,4 +13,4 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url("redis://redis:6379")
